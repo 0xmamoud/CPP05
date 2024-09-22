770c9d0f0ffcc5ab8e647046b0ae0387ce49eea4
mamoud <mamoud.antacid895@passmail.com>
2024-09-23 00:58:47 +0200

    fix: added std::endl instead of '\n'

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,55 +1,46 @@
 #include "Bureaucrat.hpp"
+#include <iostream>
 
-int main()
-{
-	std::cout << std::endl << "-------*NORMAL CASE*-------" << std::endl;
-	{
-		try
-		{
-			Bureaucrat mamoud("mamoud", 20);
-			std::cout << mamoud << std::endl;
-			mamoud.increaseGrade();
-			std::cout << mamoud << std::endl;
-			mamoud.decreaseGrade();
-			std::cout << mamoud << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
-	std::cout << std::endl << "-------*INCREASE CASE*-------" << std::endl;
-	{
-		try
-		{
-			Bureaucrat mamoud("mamoud", 1);
-			std::cout << mamoud << std::endl;
-			mamoud.increaseGrade();
-			std::cout << mamoud << std::endl;
-			mamoud.decreaseGrade();
-			std::cout << mamoud << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
-	std::cout << std::endl << "-------*DECREASE CASE*-------" << std::endl;
-	{
-		try
-		{
-			Bureaucrat mamoud("mamoud", 150);
-			std::cout << mamoud << std::endl;
-			mamoud.decreaseGrade();
-			std::cout << mamoud << std::endl;
-			mamoud.increaseGrade();
-			std::cout << mamoud << std::endl;
-		}
-		catch(const std::exception& e)
-		{
-			std::cerr << e.what() << '\n';
-		}
-	}
+int main() {
+  std::cout << std::endl << "-------*NORMAL CASE*-------" << std::endl;
+  {
+    try {
+      Bureaucrat mamoud("mamoud", 20);
+      std::cout << mamoud << std::endl;
+      mamoud.increaseGrade();
+      std::cout << mamoud << std::endl;
+      mamoud.decreaseGrade();
+      std::cout << mamoud << std::endl;
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+  std::cout << std::endl << "-------*INCREASE CASE*-------" << std::endl;
+  {
+    try {
+      Bureaucrat mamoud("mamoud", 1);
+      std::cout << mamoud << std::endl;
+      mamoud.increaseGrade();
+      std::cout << mamoud << std::endl;
+      mamoud.decreaseGrade();
+      std::cout << mamoud << std::endl;
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
+  std::cout << std::endl << "-------*DECREASE CASE*-------" << std::endl;
+  {
+    try {
+      Bureaucrat mamoud("mamoud", 150);
+      std::cout << mamoud << std::endl;
+      mamoud.decreaseGrade();
+      std::cout << mamoud << std::endl;
+      mamoud.increaseGrade();
+      std::cout << mamoud << std::endl;
+    } catch (const std::exception &e) {
+      std::cerr << e.what() << std::endl;
+    }
+  }
 
-	return 0;
+  return 0;
 }
